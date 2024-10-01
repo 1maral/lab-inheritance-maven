f63e89403fe8113671c9c256eeafd221b0ea3812
1maral <erdenemaral>
2024-10-01 14:54:40 -0500

    Add BasicCounter, along with some tests and some experiments

--- src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
@@ -1,9 +1,10 @@
 package edu.grinnell.csc207.experiments;
 
-// import edu.grinnell.csc207.util.Counter;
-// import edu.grinnell.csc207.util.BasicCounter;
+import edu.grinnell.csc207.util.Counter;
 
 import java.io.PrintWriter;
+
+import edu.grinnell.csc207.util.BasicCounter;
 
 /**
  * Some experiments as we explore our various Counter classes.
@@ -18,6 +19,25 @@ public class CounterExperiments {
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
 
+    // Set up some counters
+    Counter alpha = new BasicCounter(0);
+    Counter beta = new BasicCounter(123);
+    Counter gamma = new BasicCounter(-5);
+
+    // Print original values
+    pen.println("Original alpha = " + alpha);
+    pen.println("Original beta = " + beta);
+    pen.println("Original gamma = " + gamma);
+  
+    // Print incremented values
+    alpha.increment();
+    beta.increment();
+    gamma.increment();
+    pen.println("Updated alpha = " + alpha);
+    pen.println("Updated beta = " + beta);
+    pen.println("Updated gamma = " + gamma);
+
+    // And we're done
     pen.close();
   } // main(String[])
 } // class CounterExperiments
---
+++ src/main/java/edu/grinnell/csc207/util/BasicCounter.java
@@ -0,0 +1,47 @@
+package edu.grinnell.csc207.util;
+
+public class BasicCounter implements Counter{
+
+    /*
+     * fields
+     */
+
+    int count;
+    int start;
+
+    /*constructor
+     * 
+     */
+    
+    public BasicCounter (int val){
+     this.count = val;
+     this.start = val;
+    }
+
+    /*increment, add one to the count
+     * @exception throws exception if the count is already the maximum integer value.
+     */
+    public void increment()throws Exception{
+        this.count +=1;
+    }
+    
+    /*reset: resrs count to start
+     */
+    public void reset (){
+        this.count = this.start;
+    }
+
+    /*toString. It returns a string of the form "[" + this.count + "]".
+     * @return String result
+     */
+    public String toString(){
+        return "[" + this.count + "]";
+    }
+
+    /*get, return the value of count
+     * @return int Count
+     */
+    public int get(){
+        return this.count;
+    }
+}
